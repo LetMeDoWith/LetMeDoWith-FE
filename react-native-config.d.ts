f04459681b2f4f020ca4e602edd88b391d655f59
JinHee <327jhl@naver.com>
2024-02-12 23:31:55 +0900

    [TAS-44] chore: react-native-google-signin 패키지 설치 및 환경 변수 타입 선언, Firebase SDK 연동 관련 파일 gitignore 추가

--- react-native-config.d.ts
+++ react-native-config.d.ts
@@ -1,4 +1,6 @@
 declare module 'react-native-config' {
   export const KAKAO_APP_KEY: string;
   export const KAKAO_APP_KEY_WITH_KAKAO: string;
+  export const GOOGLE_WEB_CLIENT_ID: string;
+  export const REVERSED_CLIENT_ID: string;
 }
