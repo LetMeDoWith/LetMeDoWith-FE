f806336022fa3e13ee018c952f6172c86f53af6e
JinHee <327jhl@naver.com>
2024-03-10 17:32:13 +0900

    [TAS-74] chore: Bottom Tab 네비게이터 적용

--- App.tsx
+++ App.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
-import { Login } from '@screens/Login';
+// import { Login } from '@screens/Login';
+import { BottomTabNavigator } from 'components/navigators/BottomTabNavigator';
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView>
-      <Login />
+    <SafeAreaView style={styles.container}>
+      {/*<Login />*/}
+      <NavigationContainer>
+        <BottomTabNavigator />
+      </NavigationContainer>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+});
 
 export default App;
