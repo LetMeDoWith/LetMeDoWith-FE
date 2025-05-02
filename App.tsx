ZGenieZ/MatchMyImage-FE

[TAS-131] add: 토큰 발급 api 연동

App.tsx
@@ -1,30 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { Login } from 'screens/Login';
 import { BottomTabNavigator } from 'components/navigators/BottomTabNavigator';
 import { ThemeContext } from 'hooks/shared/useTheme';
 import { theme } from 'styles/theme';
 import { Signup } from 'screens/Signup';
+import { useAuthStore } from 'stores/auth';
 
 function App(): React.JSX.Element {
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(true);
-  const [isNeedSignUp] = useState<boolean>(true);
+  const queryClient = new QueryClient();
+  const { isLoggedIn, isNeedSignUp } = useAuthStore();
 
   return (
-    <ThemeContext.Provider value={theme}>
-      <View style={styles.container}>
-        {isLoggedIn ? (
-          <SafeAreaProvider>
-            <NavigationContainer>{isNeedSignUp ? <Signup /> : <BottomTabNavigator />}</NavigationContainer>
-          </SafeAreaProvider>
-        ) : (
-          <Login setIsLoggedIn={setIsLoggedIn} />
-        )}
-      </View>
-    </ThemeContext.Provider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeContext.Provider value={theme}>
+        <View style={styles.container}>
+          {isLoggedIn ? (
+            <SafeAreaProvider>
+              <NavigationContainer>{isNeedSignUp ? <Signup /> : <BottomTabNavigator />}</NavigationContainer>
+            </SafeAreaProvider>
+          ) : (
+            <Login />
+          )}
+        </View>
+      </ThemeContext.Provider>
+    </QueryClientProvider>
   );
 }
 

LetMeDoWith/LetMeDoWith-FE

App.tsx
@@ -1,38 +1,42 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { PaperProvider, Portal } from 'react-native-paper';
 import { KeyboardProvider } from 'react-native-keyboard-controller';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { Login } from 'screens/Login';
 import { HomeStackNavigator } from 'components/navigators/Stack/Home';
 import { ThemeContext } from 'hooks/shared/useTheme';
 import { theme } from 'styles/theme';
 import { Signup } from 'screens/Signup';
+import { useAuthStore } from 'stores/auth';
 
 function App(): React.JSX.Element {
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
-  const [isNeedSignUp] = useState<boolean>(true);
+  const queryClient = new QueryClient();
+  const { isLoggedIn, isNeedSignUp } = useAuthStore();
 
   return (
-    <ThemeContext.Provider value={theme}>
-      <PaperProvider>
-        <Portal>
-          <View style={styles.container}>
-            {!isLoggedIn ? (
-              <SafeAreaProvider>
-                <KeyboardProvider>
-                  <NavigationContainer>{isNeedSignUp ? <Signup /> : <HomeStackNavigator />}</NavigationContainer>
-                </KeyboardProvider>
-              </SafeAreaProvider>
-            ) : (
-              <Login setIsLoggedIn={setIsLoggedIn} />
-            )}
-          </View>
-        </Portal>
-      </PaperProvider>
-    </ThemeContext.Provider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeContext.Provider value={theme}>
+        <PaperProvider>
+          <Portal>
+            <View style={styles.container}>
+              {!isLoggedIn ? (
+                <SafeAreaProvider>
+                  <KeyboardProvider>
+                    <NavigationContainer>{isNeedSignUp ? <Signup /> : <HomeStackNavigator />}</NavigationContainer>
+                  </KeyboardProvider>
+                </SafeAreaProvider>
+              ) : (
+                <Login  />
+              )}
+            </View>
+          </Portal>
+        </PaperProvider>
+      </ThemeContext.Provider>
+    </QueryClientProvider>
   );
 }
 